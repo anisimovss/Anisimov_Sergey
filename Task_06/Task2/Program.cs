ef4a8e8c472e9fb1f1210bf708b13c040e70a0a5
Сергей <anisimovst1@gmail.com>
2016-02-10 23:27:10 +0400

    Task2

---
+++ Task_06/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            string str = Console.ReadLine();
+            List<string> words = new List<string>();
+            int i = 0;
+            int iStart = 0;
+            int count = 0;
+            while (i < str.Length)
+            {
+                if ((str[i] != ' ') && (str[i] != '.'))
+                {
+                    count++;
+                    if (i == str.Length - 1) words.Add(str.Substring(iStart, count));
+                    i++;
+                }
+                else
+                {
+                    if ((i != 0) && (str[i - 1] != ' ') && (str[i - 1] != '.'))
+                    {
+                        words.Add(str.Substring(iStart, count));
+                        count = 0;
+                    }
+                    i++;
+                    iStart = i;
+                }
+            }
+            count = 0;
+            for (i = 0; i < words.Count; i++)
+            {
+                if (words[i] != null)
+                {
+                    for (int j = i; j < words.Count; j++)
+                    {
+                        if ((words[j] != null) && (words[i].Equals(words[j], StringComparison.InvariantCultureIgnoreCase)))
+                        {
+                            count++;
+                            if (i!=j) words[j] = null;
+                        }
+                    }
+                    Console.WriteLine("Слово: {0} встречается {1} раз", words[i], count);
+                }
+                count = 0;
+            }
+        }
+    }
+}
