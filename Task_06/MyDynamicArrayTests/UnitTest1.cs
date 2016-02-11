66985e4ccb1b2fc4a15d76fe2a66fe2839d4657f
Сергей <anisimovst1@gmail.com>
2016-02-11 05:19:00 +0400

    Dynamic array

---
+++ Task_06/MyDynamicArrayTests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Task3;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyDynamicArrayTests
+{
+    [TestClass]
+    public class MyDynamicArrayTest
+    {
+        [TestMethod]
+        public void DefaultConstructorTest()
+        {
+            MyDynamicArray<int> a = new MyDynamicArray<int>();
+            Assert.AreEqual(8, a.Capacity);
+            Assert.AreEqual(0, a.Length);
+        }
+
+        [TestMethod]
+        public void ConstructorWithParametersTest()
+        {
+            MyDynamicArray<int> a = new MyDynamicArray<int>(15);
+            MyDynamicArray<string> b = new MyDynamicArray<string>(30);
+            Assert.AreEqual(15, a.Capacity);
+            Assert.AreEqual(0, a.Length);
+            Assert.AreEqual(30, b.Capacity);
+            Assert.AreEqual(0, b.Length);
+        }
+
+        [TestMethod]
+        public void ConstructorWithCollectionTest()
+        {
+            List<int> col = new List<int>();
+            col.Add(2);
+            col.Add(4);
+            col.Add(6);
+            col.Add(8);
+            MyDynamicArray<int> arr = new MyDynamicArray<int>(col);
+            Assert.AreEqual(4, arr.Capacity);
+            Assert.AreEqual(4, arr.Length);
+            Assert.AreEqual(arr.Length, arr.Capacity);
+            Assert.AreEqual(2, arr[0]);
+            Assert.AreEqual(4, arr[1]);
+            Assert.AreEqual(6, arr[2]);
+            Assert.AreEqual(8, arr[3]);
+        }
+
+    }
+}
---
+++ Task_06/Task3/MyDynamicArray.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class MyDynamicArray<T> : IEnumerable<T>
+    {
+        private T[] _data;
+
+        private int _length;
+        private int _capacity;
+
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+            private set
+            {
+                _length = value;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            private set
+            {
+                _capacity = value;
+            }
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if ((i < _capacity) && (i >= 0)) return _data[i];
+                else throw new ArgumentOutOfRangeException();
+            }
+            set
+            {
+                if ((i < _capacity) && (i >= 0))
+                {
+                    _length++;
+                    _data[i] = value;
+                } 
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public MyDynamicArray()
+        {
+            _capacity = 8;
+            _data = new T[_capacity];
+            _length = 0;
+        }
+
+        public MyDynamicArray(int n)
+        {
+            _capacity = n;
+            _data = new T[_capacity];
+            _length = 0;
+        }
+
+        public MyDynamicArray(IEnumerable<T> collection)
+        {
+            _data = collection.ToArray();
+            _length = _data.Length;
+            _capacity = _data.Length;
+        }
+
+        public void Add(T elem)
+        {
+            if (_length < _capacity) _data[_length] = elem;
+            else
+            {
+                _capacity = _capacity * 2;
+                Array.Resize(ref _data, _capacity);
+                _data[_length] = elem;
+            }
+            _length++;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if ((collection.Count() + _length) > _capacity)
+            {
+                _capacity = _length + collection.Count();
+                Array.Resize(ref _data, _capacity);
+            }
+            foreach (T elem in collection)
+            {
+                _data[Length] = elem;
+                Length++;
+            }
+        }
+
+        public bool Remove(T element)
+        {
+            int i = 0;
+            int index = 0;
+            bool flag = false;
+            if (_length > 0)
+            {
+                for (i = 0; i < _length; i++)
+                {
+                    if (((IComparable)(_data[i])).CompareTo(element) == 0) 
+                    {
+                        index = i;
+                        flag = true;
+                    }
+                    if (flag)
+                    {
+                        if ((i - 1) == _length)
+                        {
+                            _data[i] = default(T);
+                            return true;
+                        }
+                        else
+                        {
+                            _data[i] = _data[i + 1];
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool Insert(T element, int index)
+        {
+            if (index < _length)
+            {
+                if (_length + 1 > _capacity)
+                {
+                    _capacity = _capacity * 2;
+                    Array.Resize(ref _data, _capacity);
+                }
+                for (int i = _length; i > index; i++)
+                {
+                    _data[i] = _data[i-1];
+                }
+                _data[index] = element;
+                _length++;
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_data).GetEnumerator();
+        }
+    }
+}
