9294b74ca16486b5b4726464a4ff10130a930905
Сергей <anisimovst1@gmail.com>
2016-02-18 00:11:14 +0400

    Task1 ArrayHelper

---
+++ Task_08/Task1/ArrayHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class ArrayHelper
+    {
+        public static double SumArray(this double[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++) sum += arr[i];
+            return sum;
+        }
+    }
+}
---
+++ Task_08/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Введите количество элементов:");
+            while (!int.TryParse(Console.ReadLine(), out n)) Console.Write("Введено некорректное значение. Введите количество элементов:");
+            double[] arr = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите элемент под номером {0} :", i);
+                while (!double.TryParse(Console.ReadLine(), out arr[i])) Console.Write("Введено некорректное значение. Введите элемент под номером {0} :", i);
+            }
+            Console.WriteLine("Сумма всех элементов: " + arr.SumArray());
+            Console.ReadKey();
+        }
+    }
+}
