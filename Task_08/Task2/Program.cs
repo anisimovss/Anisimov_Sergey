8c286ee1708cbe260a708f54fea1be6d7d1f4d01
Сергей <anisimovst1@gmail.com>
2016-02-18 01:56:09 +0400

    StringHelper isPositiveNumber Task2

---
+++ Task_08/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            if (str.IsPositiveNumber()) Console.WriteLine("Эта строка является положительным целым числом");
+            else Console.WriteLine("Эта строка не является положительным целым числом");
+        }
+    }
+}
---
+++ Task_08/Task2/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class StringHelper
+    {
+        public static bool IsPositiveNumber(this string str)
+        {
+            if (Convert.ToByte('-') == str[0]) return false;
+            else {
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    if (((Convert.ToByte(str[i]) - Convert.ToByte('0')) > 9) || ((Convert.ToByte(str[i]) - Convert.ToByte('0')) < 0)) return false;
+                }
+                return true;
+            }
+        }
+    }
+}
