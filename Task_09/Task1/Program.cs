48fc7a28b62ad42fec9b2525dc1b0f871f336f5d
Сергей <anisimovst1@gmail.com>
2016-02-22 06:56:34 +0400

    Task_09 task1 the second branch

--- Task_09/Task1/Program.cs
+++ Task_09/Task1/Program.cs
@@ -9,18 +9,54 @@ namespace Task1
 {
     class Program
     {
-        static string pathFileDirectory = @"C:\....\fileplace";
-        static string pathTmpDirectory = @"C:\....\fileplace\tmplocal";
+        static string pathFileDirectory = @"C:\Users\Сергей\Desktop\avm11\epam\workspace\fileplace";
+        static string pathTmpDirectory = @"C:\Users\Сергей\Desktop\avm11\epam\workspace\fileplace\tmplocal";
 
         static void Main(string[] args)
         {
-            DirectoryInfo dir = Directory.CreateDirectory(pathTmpDirectory);
-            Console.WriteLine("start");
-            Change(pathFileDirectory);
-            Console.ReadLine();
+            //задание выполнил не правильно, не сразу увиедел что нужно сохранять папку.
+            bool flag = true;
+            int n;
+            DirectoryInfo dir = Directory.CreateDirectory(pathFileDirectory);
+            DirectoryInfo dirTmp = Directory.CreateDirectory(pathTmpDirectory);
+            while (flag)
+            {
+                Console.WriteLine("Выберите режим(номер):");
+                Console.WriteLine("1-Режим наблюдения");
+                Console.WriteLine("2-Режим отката изменений");
+                Console.WriteLine("3-Выход");
+                while (!int.TryParse(Console.ReadLine(), out n)) Console.Write("Введено некорректное значение.");
+                if (n == 1)
+                {
+                    Console.WriteLine("Режим наблюдения");
+                    Subscribe(pathFileDirectory);
+                    Console.ReadLine();
+                }
+                else if (n == 2)
+                {
+                    Console.WriteLine("Режим отката изменений");
+                    Show(dirTmp);
+                    Console.WriteLine("Введите дату и имя файла:");
+                    string str = Console.ReadLine();
+                    int i = str.Length - 1;
+                    while (str[i] != ' ') i--;
+                    File.Copy(pathTmpDirectory + "\\" + str, pathFileDirectory + "\\" + str.Substring(i + 1, str.Length - i - 1), true);
+                }
+                else if (n == 3) flag = false;
+            }
         }
 
-        static void Change(string path)
+        static void Show(DirectoryInfo dir)
+        {
+            FileInfo[] files = dir.GetFiles();
+            if (files.Length != 0)
+            {
+                Console.WriteLine("Найдено файлов: {0}", files.Length);
+                foreach (FileInfo item in files) Console.WriteLine("File: {0} ", item.Name);
+            }
+        }
+
+        static void Subscribe(string path)
         {
             FileSystemWatcher watcher = new FileSystemWatcher(path, "*.txt");
             watcher.Created += new FileSystemEventHandler(watcher_Created);
