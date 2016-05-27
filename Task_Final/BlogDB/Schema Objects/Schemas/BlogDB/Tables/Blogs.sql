2999ae3fa316dcc08db8a69525e9d5c7614e044c
Сергей <anisimovst1@gmail.com>
2016-05-27 07:09:41 +0400

    fix cascade

--- Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Blogs.sql
+++ Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Blogs.sql
@@ -4,5 +4,5 @@
     [UserID] INT NOT NULL, 
     [Document] NTEXT NOT NULL, 
     [Tag] NCHAR(10) NOT NULL, 
-    CONSTRAINT [FK_Blogs_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID]) 
+    CONSTRAINT [FK_Blogs_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID]) on delete cascade
 )
--- Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Comments.sql
+++ Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Comments.sql
@@ -5,5 +5,7 @@
     [Comment] NTEXT NOT NULL, 
     [BlogID] INT NOT NULL, 
     CONSTRAINT [FK_Comments_Blogs] FOREIGN KEY ([BlogID]) REFERENCES [BlogDB].[Blogs]([BlogID]), 
-    CONSTRAINT [FK_Comments_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID])
+    CONSTRAINT [FK_Comments_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID]) on delete cascade
 )
+
+GO
