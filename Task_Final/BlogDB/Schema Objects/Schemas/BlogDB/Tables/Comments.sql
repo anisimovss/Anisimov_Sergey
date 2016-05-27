61220f723e052acb016c7f499f032d7d62810724
Сергей <anisimovst1@gmail.com>
2016-05-28 01:11:11 +0400

    fix

--- Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Comments.sql
+++ Task_Final/BlogDB/Schema Objects/Schemas/BlogDB/Tables/Comments.sql
@@ -4,8 +4,8 @@
     [UserID] INT NOT NULL, 
     [Comment] NTEXT NOT NULL, 
     [BlogID] INT NOT NULL, 
-    CONSTRAINT [FK_Comments_Blogs] FOREIGN KEY ([BlogID]) REFERENCES [BlogDB].[Blogs]([BlogID]), 
-    CONSTRAINT [FK_Comments_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID]) on delete cascade
+    CONSTRAINT [FK_Comments_Users] FOREIGN KEY ([UserID]) REFERENCES [BlogDB].[Users]([UserID]),
+    CONSTRAINT [FK_Comments_Blogs] FOREIGN KEY ([BlogID]) REFERENCES [BlogDB].[Blogs]([BlogID]) on delete cascade
 )
 
 GO
