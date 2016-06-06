4e61f0cb8f589dde440919959b5f22fd9c62ec04
Сергей <anisimovst1@gmail.com>
2016-06-07 00:18:15 +0400

    add test data

---
+++ Task_Final/BlogDB/DataScript/BlogDB.Comments.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [BlogDB].[Comments] ON
+INSERT INTO [BlogDB].[Comments] ([CommentID], [UserID], [Comment], [BlogID]) VALUES (6, 1, N'Отличная статья!', 1)
+INSERT INTO [BlogDB].[Comments] ([CommentID], [UserID], [Comment], [BlogID]) VALUES (7, 1, N'12345678', 2)
+INSERT INTO [BlogDB].[Comments] ([CommentID], [UserID], [Comment], [BlogID]) VALUES (8, 2, N'2134', 2006)
+INSERT INTO [BlogDB].[Comments] ([CommentID], [UserID], [Comment], [BlogID]) VALUES (1019, 1, N'>sdfsdfsdfsd', 3023)
+INSERT INTO [BlogDB].[Comments] ([CommentID], [UserID], [Comment], [BlogID]) VALUES (1020, 1, N'>sdfsdfsdf sdf sd', 1)
+SET IDENTITY_INSERT [BlogDB].[Comments] OFF
---
+++ Task_Final/BlogDB/DataScript/BlogDB.Users.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [BlogDB].[Users] ON
+INSERT INTO [BlogDB].[Users] ([UserID], [Login], [Password]) VALUES (1, N'root      ', N'1234      ')
+INSERT INTO [BlogDB].[Users] ([UserID], [Login], [Password]) VALUES (2, N'first     ', N'111       ')
+SET IDENTITY_INSERT [BlogDB].[Users] OFF
